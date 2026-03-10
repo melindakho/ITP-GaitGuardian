681cfdd931fe47a4208cff2ab65b6339892fd6fc
melindakho <melinda.khosasih@singaporetech.edu.sg>
2026-03-10 21:49:55 +0800

    fix: add arm64-v8a ABI filter to fix app crash on startup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        ndk {
+            abiFilters += listOf("arm64-v8a")
+        }
     }
 
     buildTypes {
